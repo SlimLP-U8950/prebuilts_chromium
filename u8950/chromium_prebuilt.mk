57eb45dfc02c6f8c7b4ac9b382ab8845661e095a
ksrt12 <kazakov12stepan2012@live.ru>
2015-09-28 13:51:35 +0600

    Init commit

---
+++ u8950/chromium_prebuilt.mk
@@ -0,0 +1,45 @@
+# Copyright (C) 2014 The OmniROM Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := prebuilts/chromium/u8950/
+ARCH := arm
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/app/webview/webview.apk:system/app/webview/webview.apk \
+    $(LOCAL_PATH)/lib/libwebviewchromium.so:system/lib/libwebviewchromium.so \
+    $(LOCAL_PATH)/lib/libwebviewchromium_plat_support.so:system/lib/libwebviewchromium_plat_support.so \
+    $(LOCAL_PATH)/lib/libwebviewchromium_loader.so:system/lib/libwebviewchromium_loader.so
+
+ifeq ($(ARCH),arm64)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/lib64/libwebviewchromium.so:system/lib64/libwebviewchromium.so \
+    $(LOCAL_PATH)/lib64/libwebviewchromium_plat_support.so:system/lib64/libwebviewchromium_plat_support.so \
+    $(LOCAL_PATH)/lib64/libwebviewchromium_loader.so:system/lib64/libwebviewchromium_loader.so
+endif
+
+ifeq ($(ARCH),arm)
+$(shell mkdir -p out/target/product/u8950/system/app/webview/lib/arm/)
+$(shell cp -r $(LOCAL_PATH)/app/webview/lib/arm/libwebviewchromium.so out/target/product/u8950/system/app/webview/lib/arm/libwebviewchromium.so)
+endif
+
+ifeq ($(ARCH),arm64)
+$(shell mkdir -p out/target/product/u8950/system/app/webview/lib/arm64/)
+$(shell cp -r $(LOCAL_PATH)/app/webview/lib/arm64/libwebviewchromium.so out/target/product/u8950/system/app/webview/lib/arm64/libwebviewchromium.so)
+endif
+
+ifeq ($(ARCH),x86)
+$(shell mkdir -p out/target/product/u8950/system/app/webview/lib/x86/)
+$(shell cp -r $(LOCAL_PATH)/app/webview/lib/x86/libwebviewchromium.so out/target/product/u8950/system/app/webview/lib/x86/libwebviewchromium.so)
+endif
+
